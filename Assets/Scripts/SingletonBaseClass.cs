e2058bf55e58fceef0a2174636eeed4b8265ee4a
alfianAH <alfian.hamdani04@gmail.com>
2022-01-29 14:55:50 +0800

    Initial commit

---
+++ Assets/Scripts/SingletonBaseClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SingletonBaseClass<T>: MonoBehaviour where T: MonoBehaviour 
+{
+    protected static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            string log = typeof(T).Name;
+
+            if (instance != null) return instance;
+            
+            instance = FindObjectOfType<T>();
+            if (instance == null)
+            {
+                Debug.LogError($"{log} not found");
+            }
+
+            return instance;
+        }
+    }
+}
