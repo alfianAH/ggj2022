cd8e05b7b3a7ac04c577a9db7652ac47b16dc8e3
alfianAH <alfian.hamdani04@gmail.com>
2022-01-30 15:35:28 +0800

    Add choose options

--- Assets/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -1,13 +1,34 @@
+using System.Collections.Generic;
 using Box;
+using Effects;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gameplay
 {
     public class GameplayManager : SingletonBaseClass<GameplayManager>
     {
+        public List<BoxSprite> boxSprites;
         public BoxPersonality chosenBoxPersonality;
+        [SerializeField] private Button happyButton, madButton;
+        [SerializeField] private Image removeButtonSprite;
+        [SerializeField] private CanvasGroup choosePanel;
 
-        public void ChooseBoxPersonality(BoxPersonality boxPersonality){
+        private void Awake() {
+            StartCoroutine(FadingEffect.FadeIn(choosePanel));
+            happyButton.onClick.AddListener(() => ChooseBoxPersonality(BoxPersonality.Positive));
+            madButton.onClick.AddListener(() => ChooseBoxPersonality(BoxPersonality.Negative));
+        }
+
+        private void ChooseBoxPersonality(BoxPersonality boxPersonality){
             chosenBoxPersonality = boxPersonality;
+            // Change button sprite
+            foreach(BoxSprite boxSprite in boxSprites){
+                if(boxSprite.boxPersonality == boxPersonality){
+                    removeButtonSprite.sprite = boxSprite.personalitySprite;
+                }
+            }
+            StartCoroutine(FadingEffect.FadeOut(choosePanel));
         }
     }
 }
